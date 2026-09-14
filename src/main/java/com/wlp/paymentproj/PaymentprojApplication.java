3a0f7ecbf3a262448e3bc9710afe4fa7a0e85828
RIDDHIMA BHALLA <rid28bhalla@gmail.com>
2026-09-14 22:10:44 +0530

    spring boot initialise setup

---
+++ src/main/java/com/wlp/paymentproj/PaymentprojApplication.java
@@ -0,0 +1,13 @@
+package com.wlp.paymentproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentprojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentprojApplication.class, args);
+	}
+
+}
